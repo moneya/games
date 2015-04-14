28aebc7c78fbae12e7c9071161c1d4665ccd4352
moneya <moneya@users.noreply.github.com>
2015-04-14 16:00:19 +0000

    Update menu3.php

--- page/menu3.php
+++ page/menu3.php
@@ -2,6 +2,6 @@
 <body bgcolor="#EEE">
 
 <!-- BEGIN - EMBED Channel FROM Come2Play.com -->
-<iframe src="http://www.come2play.com/appGame/AllGames.aspx?channel_id=971583" width="100%" height="1230" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
+<iframe src="http://www.come2play.com/appGame/AllGames.aspx?channel_id=971583" width="100%" height="1200" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
 <!-- END - EMBED Channel FROM Come2Play.com -->
 </body>
