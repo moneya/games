cd5db9f7e959796ce29845f5a4ff39e13e8ba2de
moneya <moneya@users.noreply.github.com>
2015-04-16 07:14:24 +0000

    fixed a minor bug

--- page/menu3.php
+++ page/menu3.php
@@ -2,6 +2,6 @@
 <body bgcolor="#EEE">
 
 <!-- BEGIN - EMBED Channel FROM Come2Play.com -->
-<iframe src="http://www.come2play.com/appGame/AllGames.aspx?channel_id=971629" width="100%" height="1200" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
+<iframe src="http://http://game.getdino.com/socialgame.php" width="100%" height="1200" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
 <!-- END - EMBED Channel FROM Come2Play.com -->
 </body>
